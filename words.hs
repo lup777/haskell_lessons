5fc6c94f32ffa3f7a628063fb7f1d00e5ba478aa
Alexander Lapshenkov <alexander.lapshenkov@lge.com>
2018-07-16 12:29:29 +0300

    add: check random word (ask as for english)

--- words.hs
+++ words.hs
@@ -4,6 +4,8 @@ import System.IO
 import Prelude hiding (putStr)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.UTF8 as BU
+import System.Random
+import Data.List
 
 data RuString = RuString String
 
@@ -18,5 +20,38 @@ instance Show RuString where
 
 vac :: [Entry]
 vac = [(Entry ["have a go"] [RuString "попытаться"] ["try"]),
-       (Entry ["have a go"] [RuString "попытаться"] ["try"])]
-main = putStrLn $ show vac
+       (Entry ["come up with"] [RuString "придумывать"] [""]),
+       (Entry ["get on"] [RuString "садиться на, садиться в транспортное средство",
+                          RuString "звонить/поднять трубку",
+                          RuString "двигаться дальше",
+                          RuString "достигать"] [""])]
+
+getRandomVacEntry :: IO Entry
+getRandomVacEntry = (randomRIO (0, (length vac) - 1 ) :: IO Int) >>= \num -> return (vac !! num)
+
+
+
+checkRandomWord :: IO ()
+checkRandomWord = getRandomVacEntry >>= \entry -> checkWord entry
+
+checkWord :: Entry -> IO ()
+checkWord entry = checkEnglish entry >>=
+  \result -> if result == True then putStrLn $ "You are Right :-)   -> " ++ (show entry)
+             else                   putStrLn $ "You are not Right   -> " ++ (show entry)
+
+checkEnglish :: Entry -> IO Bool
+checkEnglish entry = requestEnglish entry >>= \answer -> return $ (isInVac answer (english entry))
+
+requestEnglish :: Entry -> IO String
+requestEnglish entry = do putStr ("== " ++ (show . local $ entry ) ++ " ? ")
+                          getLine
+
+isInVac :: String -> [String] -> Bool
+isInVac str [] = False
+isInVac str (x:xs)
+  | (isSubsequenceOf str x) == True = True
+  | otherwise                       = isInVac str xs 
+-- TO DO !!!!!!!!
+
+main = checkRandomWord >> putStrLn ""
+
